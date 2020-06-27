e60e7f1bb525e47b848631d7e642acfef222c4c9
Shobir <33521457+ShobirIrsan@users.noreply.github.com>
2020-06-27 09:07:06 +0700

    file flutter
    flutter

---
+++ httpCoba.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:http/http.dart' as http;
+
+
+class Kirim {
+  Future send() async {
+    var url = 'http://192.168.11.4/dpot?a=100';
+    var response = await http.get(url);
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      print('sukses');
+    } else {
+      print('gagal');
+    }
+  }
+}
